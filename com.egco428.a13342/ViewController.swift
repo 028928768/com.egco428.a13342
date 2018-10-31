e91afd61415196028e08539ef881043dcf078351
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-10-31 13:52:54 +0700

    Initial Commit

---
+++ com.egco428.a13342/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  com.egco428.a13342
+//
+//  Created by Kanta'MacPro on 31/10/2561 BE.
+//  Copyright © 2561 Kanta'MacPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
