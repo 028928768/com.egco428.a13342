5663fe628c42bc314de8a25b800e75bab90adbc5
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-10-31 14:43:25 +0700

    initial project

---
+++ com.egco428.a13342/CookieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CookieTableViewCell.swift
+//  com.egco428.a13342
+//
+//  Created by Kanta'MacPro on 31/10/2561 BE.
+//  Copyright © 2561 Kanta'MacPro. All rights reserved.
+//
+
+import UIKit
+
+class CookieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
