6da08ce5db6f6208c022da2fdbe7f7fa7b6f0789
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-10-31 22:11:12 +0700

    add delete implement

---
+++ com.egco428.a13342/AddDeleteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddDeleteViewController.swift
+//  com.egco428.a13342
+//
+//  Created by Kanta'MacPro on 31/10/2561 BE.
+//  Copyright © 2561 Kanta'MacPro. All rights reserved.
+//
+
+import UIKit
+
+class AddDeleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
